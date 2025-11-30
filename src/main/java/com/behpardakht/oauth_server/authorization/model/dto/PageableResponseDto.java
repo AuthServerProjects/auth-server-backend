3ea17f56a4f793097988777751284558b2d1b3ea
Rosa <raziyeh.sharifi7@gmail.com>
2025-11-30 15:36:40 +0330

    add base filter spec

---
+++ src/main/java/com/behpardakht/oauth_server/authorization/model/dto/PageableResponseDto.java
@@ -0,0 +1,27 @@
+package com.behpardakht.oauth_server.authorization.model.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Data
+@Builder
+public class PageableResponseDto<T> {
+    private List<T> content;
+    private long totalElements;
+    private int totalPages;
+    private int pageNumber;
+    private int pageSize;
+
+    public static <T> PageableResponseDto<T> build(List<T> response, Page<?> page) {
+        return PageableResponseDto.<T>builder()
+                .content(response)
+                .totalElements(page.getTotalElements())
+                .totalPages(page.getTotalPages())
+                .pageNumber(page.getNumber())
+                .pageSize(page.getSize())
+                .build();
+    }
+}
