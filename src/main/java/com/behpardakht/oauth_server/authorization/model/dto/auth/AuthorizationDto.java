ccb2a94ae5f1a6c67f482bdb8de0e28012a70fe1
Rosa <raziyeh.sharifi7@gmail.com>
2025-12-06 14:58:36 +0330

    dtos handwriting

--- src/main/java/com/behpardakht/oauth_server/authorization/model/dto/auth/AuthorizationDto.java
+++ src/main/java/com/behpardakht/oauth_server/authorization/model/dto/auth/AuthorizationDto.java
@@ -1,12 +1,16 @@
 package com.behpardakht.oauth_server.authorization.model.dto.auth;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthorizationDto {
     private Long id;
     private String authorizationId;
--- src/main/java/com/behpardakht/oauth_server/authorization/model/dto/otp/VerifyOtpResponseDto.java
+++ src/main/java/com/behpardakht/oauth_server/authorization/model/dto/otp/VerifyOtpResponseDto.java
@@ -1,10 +1,14 @@
 package com.behpardakht.oauth_server.authorization.model.dto.otp;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class VerifyOtpResponseDto {
     private String state;
     private String redirectUri;
