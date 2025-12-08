e5edbb3301d81e12cfc965caa1949fa99adbd443
Rosa <raziyeh.sharifi7@gmail.com>
2025-12-08 09:37:58 +0330

    client service handwriting

--- src/main/java/com/behpardakht/oauth_server/authorization/service/ClientService.java
+++ src/main/java/com/behpardakht/oauth_server/authorization/service/ClientService.java
@@ -19,7 +19,6 @@ import org.springframework.security.oauth2.server.authorization.client.Registere
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
