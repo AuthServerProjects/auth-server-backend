e2c1c2e389749f797bd067537c2a5f5f1482b44b
Rosa <raziyeh.sharifi7@gmail.com>
2025-12-17 17:52:50 +0330

    update OAuth2TokenCustomizerImpl for new models

--- src/main/java/com/behpardakht/oauth_server/authorization/security/authorizationServer/OAuth2TokenCustomizerImpl.java
+++ src/main/java/com/behpardakht/oauth_server/authorization/security/authorizationServer/OAuth2TokenCustomizerImpl.java
@@ -2,7 +2,9 @@ package com.behpardakht.oauth_server.authorization.security.authorizationServer;
 
 import com.behpardakht.oauth_server.authorization.model.entity.Client;
 import com.behpardakht.oauth_server.authorization.model.entity.Permission;
+import com.behpardakht.oauth_server.authorization.model.entity.UserClientAssignment;
 import com.behpardakht.oauth_server.authorization.model.entity.UserRoleAssignment;
+import com.behpardakht.oauth_server.authorization.repository.UserClientAssignmentRepository;
 import com.behpardakht.oauth_server.authorization.service.ClientService;
 import com.behpardakht.oauth_server.authorization.service.MetricsService;
 import com.behpardakht.oauth_server.authorization.service.user.UserRoleAssignmentService;
@@ -15,6 +17,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static com.behpardakht.oauth_server.authorization.util.GeneralUtil.DEFAULT_CLIENT_ID;
+
 @Component
 @RequiredArgsConstructor
 public class OAuth2TokenCustomizerImpl implements OAuth2TokenCustomizer<JwtEncodingContext> {
@@ -25,10 +29,12 @@ public class OAuth2TokenCustomizerImpl implements OAuth2TokenCustomizer<JwtEncod
     private static final String CLIENT_DB_ID_CLAIM = "client-db-id";
     private static final String ROLES_CLAIM = "roles";
     private static final String AUTHORITIES_CLAIM = "authorities";
+    private static final String ASSIGNED_CLIENT_IDS_CLAIM = "assignedClientIds";
 
     private final ClientService clientService;
     private final MetricsService metricsService;
     private final UserRoleAssignmentService userRoleAssignmentService;
+    private final UserClientAssignmentRepository userClientAssignmentRepository;
 
     @Override
     public void customize(JwtEncodingContext context) {
@@ -40,21 +46,54 @@ public class OAuth2TokenCustomizerImpl implements OAuth2TokenCustomizer<JwtEncod
 
         if (ACCESS_TOKEN.equals(context.getTokenType().getValue())) {
             String username = context.getPrincipal().getName();
+
             Set<String> roles = new HashSet<>();
             Set<String> authorities = new HashSet<>();
-            List<UserRoleAssignment> assignments = userRoleAssignmentService.findByUsernameAndClientId(username, client.getId());
-            for (UserRoleAssignment assignment : assignments) {
-                roles.add(assignment.getRole().getName());
-                for (Permission permission : assignment.getRole().getPermissions()) {
-                    authorities.add(permission.getName());
-                }
+
+            if (isAdminPanel(clientId)) {
+                customizeForAdmin(context, username, roles, authorities);
+            } else {
+                customizeForUser(username, client, roles, authorities);
             }
+
             context.getClaims().claim(ROLES_CLAIM, roles);
             context.getClaims().claim(AUTHORITIES_CLAIM, authorities);
             metricsService.incrementTokenIssued(clientId, context.getAuthorizationGrantType().getValue());
         }
         if (REFRESH_TOKEN.equals(context.getTokenType().getValue())) {
             metricsService.incrementTokenRefreshed(clientId);
+        }
+    }
+
+    private boolean isAdminPanel(String clientId) {
+        return DEFAULT_CLIENT_ID.equals(clientId);
+    }
+
+    private void customizeForAdmin(JwtEncodingContext context, String username, Set<String> roles, Set<String> authorities) {
+        Set<Long> assignedClientIds = new HashSet<>();
+        List<UserClientAssignment> userClientAssignmentList = userClientAssignmentRepository.findByUserUsername(username);
+        for (UserClientAssignment userClientAssignment : userClientAssignmentList) {
+            Long assignedClientId = userClientAssignment.getClient().getId();
+            assignedClientIds.add(assignedClientId);
+
+            for (UserRoleAssignment userRoleAssignment : userClientAssignment.getUserRoleAssignments()) {
+                roles.add(userRoleAssignment.getRole().getName());
+
+                for (Permission permission : userRoleAssignment.getRole().getPermissions()) {
+                    authorities.add(assignedClientId + ":" + permission.getName());
+                }
+            }
+        }
+        context.getClaims().claim(ASSIGNED_CLIENT_IDS_CLAIM, assignedClientIds);
+    }
+
+    private void customizeForUser(String username, Client client, Set<String> roles, Set<String> authorities) {
+        List<UserRoleAssignment> userRoleAssignmentList = userRoleAssignmentService.findByUsernameAndClientId(username, client.getId());
+        for (UserRoleAssignment userRoleAssignment : userRoleAssignmentList) {
+            roles.add(userRoleAssignment.getRole().getName());
+            for (Permission permission : userRoleAssignment.getRole().getPermissions()) {
+                authorities.add(permission.getName());
+            }
         }
     }
 }
