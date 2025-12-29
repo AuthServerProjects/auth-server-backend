e32639efa9bf6e8c9f1061e08fe721c593ac0273
Rosa <raziyeh.sharifi7@gmail.com>
2025-12-29 17:19:32 +0330

    fix locale

--- src/main/java/com/behpardakht/oauth_server/authorization/config/bundle/BundleConfig.java
+++ src/main/java/com/behpardakht/oauth_server/authorization/config/bundle/BundleConfig.java
@@ -1,10 +1,12 @@
 package com.behpardakht.oauth_server.authorization.config.bundle;
 
 import com.behpardakht.oauth_server.authorization.config.Properties;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
@@ -27,5 +29,13 @@ public class BundleConfig {
     @Bean
     public LocaleResolver localeResolver() {
         return new FixedLocaleResolver(new Locale(properties.getLocalization().getLanguage()));
+    }
+
+    @PostConstruct
+    public void init() {
+        // Set the default locale for the entire application
+        Locale locale = new Locale(properties.getLocalization().getLanguage());
+        Locale.setDefault(locale);
+        LocaleContextHolder.setDefaultLocale(locale);
     }
 }
